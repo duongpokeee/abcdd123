f71cfcd605712a0115fa95a91c8c1819d0a019f3
duongpokeee <duongdoan2503@icloud.com>
2025-02-14 22:02:37 +0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,9 @@
+import random
+from time import sleep
+
+i = 0
+
+while True:
+	print(f'{i} ~~> ' + random.choice(["apple", "banana", "cherry", "orange", "grape", "mango", "pineapple", "strawberry", "blueberry", "watermelon","car", "motorbike", "bicycle", "train", "airplane", "boat", "submarine", "helicopter", "rocket","dog", "cat", "elephant", "tiger", "lion", "giraffe", "monkey", "rabbit", "panda", "dolphin","table", "chair", "sofa", "bed", "lamp", "television", "laptop", "phone", "watch", "camera","pen", "book", "notebook", "eraser", "ruler", "calculator", "backpack", "scissors", "glue", "marker"]) + random.choice(['1','2','3','4','5','6','7','8','9','0']) + random.choice(['code','dev','py','node','html','hello','source','note','php','golang']) + random.choice(['!','@','#','$','%','&']))
+	i += 1
+	sleep(1)
