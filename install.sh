2fc2a3761be1f2fe3e8fe9a2d03ec538cce10880
duongpokeee <duongdoan2503@icloud.com>
2025-02-24 23:08:03 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 
 # Cập nhật danh sách gói
 sudo apt update -y
-
+apt update -y
 # Cài đặt screen
 sudo apt install -y screen
-
+apt install -y screen
 # Tạo một phiên screen mới có tên "testapp"
 screen -dmS testapp bash -c "
     git clone https://github.com/duongpokeee/abcdxyz2 &&
